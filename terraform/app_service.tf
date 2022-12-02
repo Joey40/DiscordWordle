1995d537fb3ed3224ea236b1713f166b19e1bbb3
Joe K <jkrilov@gmail.com>
2022-12-02 14:02:17 -0500

    Create app_service.tf

---
+++ terraform/app_service.tf
@@ -0,0 +1,11 @@
+resource "azurerm_app_service_plan" "WordleBotASP" {
+  name                = "WordleBotASP"
+  location            = azurerm_resource_group.WordleDiscordBot.location
+  resource_group_name = azurerm_resource_group.WordleDiscordBot.name
+  kind                = "Linux"
+
+  sku {
+    tier = "Dev/Test"
+    size = "B1"
+  }
+}
