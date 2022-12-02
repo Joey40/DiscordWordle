0c69b427b9f3332a180edf154c8f961721db347e
Joe K <jkrilov@gmail.com>
2022-12-02 10:04:50 -0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -15,10 +15,6 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id            = "2608623e-326e-4765-95fc-b8f65a5c3cb1"
-  tenant_id                  = "d1993901-bb51-492f-8e0d-572b1384f3a6"
-  client_id                  = "7044decd-d37e-4fe5-a25f-dcb2ea341213"
-  client_secret              = var.client_secret
   skip_provider_registration = true
   features {}
 }
