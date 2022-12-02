2f85441f261c26e9b2724cf142599e6b516a727d
Joe K <jkrilov@gmail.com>
2022-12-02 10:52:24 -0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,15 +6,12 @@ terraform {
     }
   }
   backend "azurerm" {
-    config = {
-      storage_account_name = "jkrilovtfstate"
-      container_name       = "state"
-      key                  = "terraform.tfstate"
-    }
+    storage_account_name = "jkrilovtfstate"
+    container_name       = "state"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  skip_provider_registration = true
   features {}
 }
