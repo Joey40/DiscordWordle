38904052bfe31eafb22cbe2271ae54cc56eb7ef4
Joe K <jkrilov@gmail.com>
2022-12-02 12:40:46 -0500

    Update postgresql.tf

--- terraform/postgresql.tf
+++ terraform/postgresql.tf
@@ -16,5 +16,5 @@ resource "azurerm_postgresql_server" "wordle-psql" {
 
   public_network_access_enabled    = false
   ssl_enforcement_enabled          = false
-  ssl_minimal_tls_version_enforced = "TLS1_2"
+  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
 }
