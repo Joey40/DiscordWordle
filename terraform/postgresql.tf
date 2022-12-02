9233589c2a435d1d41c56ebc78f08cd39dccce50
Joe K <jkrilov@gmail.com>
2022-12-02 14:15:23 -0500

    Update postgresql.tf

--- terraform/postgresql.tf
+++ terraform/postgresql.tf
@@ -5,7 +5,7 @@ resource "azurerm_postgresql_flexible_server" "wordle-psql" {
   version                = "14"
   administrator_login    = "psqladmin"
   administrator_password = "invalidpwd"
-  zone                   = "1"
+  zone                   = "2"
 
   storage_mb = 32768
 
