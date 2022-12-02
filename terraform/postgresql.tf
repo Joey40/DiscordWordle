98929025c804d125532c977a06c1b7e6f792cef1
Joe K <jkrilov@gmail.com>
2022-12-02 13:08:36 -0500

    Update postgresql.tf

--- terraform/postgresql.tf
+++ terraform/postgresql.tf
@@ -3,7 +3,6 @@ resource "azurerm_postgresql_flexible_server" "wordle-psql" {
   resource_group_name    = azurerm_resource_group.WordleDiscordBot.name
   location               = azurerm_resource_group.WordleDiscordBot.location
   version                = "14"
-  delegated_subnet_id    = azurerm_subnet.WordlePSQLSub.id
   administrator_login    = "psqladmin"
   administrator_password = "invalidpwd"
 
