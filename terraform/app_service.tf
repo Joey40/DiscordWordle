4231389d89406cae85f3dfd09ac13260d633cde6
Joe K <jkrilov@gmail.com>
2022-12-02 20:19:28 -0500

    Update app_service.tf

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -2,11 +2,11 @@ resource "azurerm_service_plan" "WordleBotASP" {
   name                = "WordleBotASP"
   location            = azurerm_resource_group.WordleDiscordBot.location
   resource_group_name = azurerm_resource_group.WordleDiscordBot.name
-  os_type             = "Windows"
+  os_type             = "Linux"
   sku_name            = "B1"
 }
 
-resource "azurerm_windows_web_app" "WordleBotApp" {
+resource "azurerm_linux_web_app" "WordleBotApp" {
   name                = "WordleBotApp"
   resource_group_name = azurerm_resource_group.WordleDiscordBot.name
   location            = azurerm_resource_group.WordleDiscordBot.location
